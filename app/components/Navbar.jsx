3b44134a8f2161dc1d22af47e538d1f20a92b1b1
l-shahbil <laith.715.asad@gmail.com>
2025-01-29 14:06:11 +0300

    Style , Fonts and Image

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
+import logo from './dojo-logo.png'
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
     <nav>
+        <Image 
+        src={logo}
+        alt='Dojo Helpdisk Logo'
+        width={70}
+        quality={100}
+        placeholder='blur'
+        />
         <h1>Dojo Helpdisk</h1>
         <Link href="/">Dashboard</Link>
         <Link href="/tickets">Tickets</Link>
