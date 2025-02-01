09cbc855a87b70a8437fb6fbb1317c5e89ef0a13
l-shahbil <laith.715.asad@gmail.com>
2025-02-01 14:45:44 +0300

    Group Layout

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+      <main>
+          <h2 className="text-center">Login</h2>
+      </main>
+    )
+  }
+  
---
+++ app/(auth)/singup/page.jsx
@@ -0,0 +1,7 @@
+export default function Singup() {
+  return (
+    <main>
+        <h2 className="text-center">Sing Up</h2>
+    </main>
+  )
+}
