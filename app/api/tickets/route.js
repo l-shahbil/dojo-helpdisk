935a3d4e5341d7818bea55309dcff42615de5882
l-shahbil <laith.715.asad@gmail.com>
2025-02-03 00:38:00 +0300

    Fetching Data From Supabase

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,30 +1,24 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"
+import { cookies } from 'next/headers'
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 
-export const dynamic = 'force-dynamic'
-export async function GET() {
-    const res = await fetch("http://localhost:4000/tickets", {
+export async function POST(request) {
+    const ticket = await request.json()
 
-    });
-    const tickets = await res.json();
+    // get supabase instance
+    const supabase = createRouteHandlerClient({ cookies })
 
-    return NextResponse.json(tickets, {
-        status: 200
-    });
+    // get current user session
+    const { data: { session } } = await supabase.auth.getSession()
 
-}
+    // insert the data
+    const { data, error } = await supabase.from('Tickets')
+        .insert({
+            ...ticket,
+            user_email: session.user.email,
+        })
+        .select()
+        .single()
 
-export async function POST(params) {
-    const ticket = await params.json();
-
-    const res = await fetch("http://localhost:4000/tickets", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(ticket)
-    });
-
-    const newTicket = await res.json()
-
-    return NextResponse.json(newTicket, {
-        status: 201
-    });
+    return NextResponse.json({ data, error })
 }
