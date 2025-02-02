12e6e6f92a0da61981b69073c8561cfad193138a
l-shahbil <laith.715.asad@gmail.com>
2025-02-02 10:10:28 +0300

    Making the auth form

--- app/(auth)/singup/page.jsx
+++ app/(auth)/singup/page.jsx
@@ -1,7 +1,18 @@
+"use client"
+
+import AuthForm from "../authForm";
+
 export default function Singup() {
+
+  const handleSubmit =async (e,email,password)=>{
+    e.preventDefault();
+    console.log("user sign-up",email,password)
+  }
+
   return (
     <main>
         <h2 className="text-center">Sing Up</h2>
+        <AuthForm handleSubmit={handleSubmit}/>
     </main>
   )
 }
