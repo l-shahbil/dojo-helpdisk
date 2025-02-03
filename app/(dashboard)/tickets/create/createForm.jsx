428c3bc158f827288875cd624a8960e988ba8934
l-shahbil <laith.715.asad@gmail.com>
2025-02-03 16:49:25 +0300

    using useFormStatus

--- app/(dashboard)/tickets/create/createForm.jsx
+++ app/(dashboard)/tickets/create/createForm.jsx
@@ -1,11 +1,9 @@
 "use client"
 
-import { useState } from "react"
 import { addTicket } from "../action"
+import SubmitButton from "@/app/components/SubmitButton"
 
 export default function CreateForm() {
-
-  const [isLoading, setIsLoading] = useState(false)
 
   return (
     <form action={addTicket} className="w-1/2">
@@ -34,13 +32,7 @@ export default function CreateForm() {
           <option value="high">High Priority</option>
         </select>
       </label>
-      <button 
-        className="btn-primary" 
-        disabled={isLoading}
-      >
-      {isLoading && <span>Adding...</span>}
-      {!isLoading && <span>Add Ticket</span>}
-    </button>
+      <SubmitButton />
     </form>
   )
 }
---
+++ app/components/SubmitButton.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import { experimental_useFormStatus as useFormStatus } from 'react-dom'
+
+
+export default function SubmitButton() {
+    const {pending} = useFormStatus()
+
+  return (
+    <button disabled={pending} className='btn-primary'>
+        {pending && <span>Submitting...</span>} 
+        {!pending && <span>Submit</span>} 
+    </button>
+  )
+}
